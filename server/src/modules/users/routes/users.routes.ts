b1b9e335de1d06729f4b0b0bea4dffc9b42fbee6
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-02-16 17:40:32 -0300

    added controller to abstract route

---
+++ server/src/modules/users/routes/users.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '@config/upload';
+
+import UsersController from '@modules/users/controllers/UsersController';
+import UserAvatarController from '@modules/users/controllers/UserAvatarController';
+
+import ensureAuthenticated from '@shared/middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+usersRouter.get('/', usersController.show);
+
+usersRouter.post('/', usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default usersRouter;
--- server/src/shared/routes/index.ts
+++ server/src/shared/routes/index.ts
@@ -1,19 +1,11 @@
 import { Router } from 'express';
 
-import usersRouter from './users.routes';
-import sessionsRouter from './sessions.routes';
-
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-
-// rota de teste para pessoa autenticada
-routes.use(ensureAuthenticated);
-routes.get('/only-authenticated-users', (req, res) => {
-  return res.json({ message: 'hello world' });
-});
 
 export default routes;
