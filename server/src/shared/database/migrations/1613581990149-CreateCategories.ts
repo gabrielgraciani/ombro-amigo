94a632bb6506ffcc9ca87646001480b94989d8d6
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-02-17 15:38:25 -0300

    created posts migration

--- server/src/shared/database/migrations/1613581990149-CreateCategories.ts
+++ server/src/shared/database/migrations/1613581990149-CreateCategories.ts
@@ -9,7 +9,7 @@ export default class CreateCategories1613581990149
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
