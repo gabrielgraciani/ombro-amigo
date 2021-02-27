4374cbd7440bdc53974542bfd12d2cd5c5687eb4
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-02-27 10:44:59 -0300

    adding websocket to project

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,16 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import { createServer } from 'http';
+import { Socket } from 'socket.io';
+
 import '@shared/database';
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 
 import routes from '@shared/routes';
+
+import { joinUser, removeUser } from './shared/websocket/functions';
 
 const app = express();
 
@@ -33,6 +38,61 @@ app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   });
 });
 
-app.listen(process.env.PORT || 3333, () => {
+// app.listen(process.env.PORT || 3333, () => {
+//   console.log(`server started on port ${process.env.PORT || 3333}`);
+// });
+
+const server = createServer(app);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const io = require('socket.io')(server, {
+  cors,
+  transports: ['websocket', 'polling', 'flashsocket'],
+});
+
+interface RoomProps {
+  username: string;
+  roomName: string;
+}
+
+interface MessageProps {
+  value: string;
+  user: string;
+}
+
+let thisRoom = '';
+io.on('connection', (socket: Socket) => {
+  console.log('[IO] Connection => Server has a new connection');
+
+  socket.on('join room', (data: RoomProps) => {
+    console.log('in room', data);
+    const Newuser = joinUser(socket.id, data.username, data.roomName);
+    // io.to(Newuser.roomname).emit('send data' , {username : Newuser.username,roomname : Newuser.roomname, id : socket.id})
+    // io.to(socket.id).emit('send data' , {id : socket.id ,username:Newuser.username, roomname : Newuser.roomname });
+    socket.emit('send data', {
+      id: socket.id,
+      username: Newuser.username,
+      roomname: Newuser.roomname,
+    });
+
+    thisRoom = Newuser.roomname;
+    console.log(Newuser);
+    socket.join(Newuser.roomname);
+  });
+  socket.on('chat message', (data: MessageProps) => {
+    console.log('chegou aqui message', data);
+    io.to(thisRoom).emit('chat message', { data, id: socket.id });
+  });
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+    console.log(user);
+    if (user) {
+      console.log(`${user.username} has left`);
+    }
+    console.log('disconnected');
+  });
+});
+
+server.listen(process.env.PORT || 3333, () => {
   console.log(`server started on port ${process.env.PORT || 3333}`);
 });
---
+++ server/src/shared/websocket/functions.ts
@@ -0,0 +1,27 @@
+interface User {
+  socketID: string;
+  username: string;
+  roomname: string;
+}
+
+const users: User[] = [];
+function joinUser(socketId: string, userName: string, roomName: string): User {
+  const user = {
+    socketID: socketId,
+    username: userName,
+    roomname: roomName,
+  };
+  users.push(user);
+  return user;
+}
+
+function removeUser(id: string): User | undefined {
+  const index = users.findIndex(user => user.socketID === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+
+  return undefined;
+}
+
+export { joinUser, removeUser };
